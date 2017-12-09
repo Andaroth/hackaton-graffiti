853c38857733481ead8ba3ed41229d632f625c3d
Anda <ax.fiolle@gmail.com>
2017-12-09 18:15:47 +0100

    new file

---
+++ artist.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<?php
+  include("./php/models/config.php");
+  include(C."session.php");
+?>
+<!--
+	Helios by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html lang="fr">
+	<?php include(V."head.php"); ?>
+	<body class="homepage">
+		<div id="page-wrapper">
+
+			<?php include(V."header.php"); ?>
+
+			<?php include(V."banner.php"); ?>
+
+			<?php include(V."carousel.php"); ?>
+
+			<?php include(V."main.php"); ?>
+
+			<?php include(V."features.php"); ?>
+
+			<?php include(V."footer.php"); ?>
+				
+
+		</div>
+
+		<?php include(V."jscript.php"); ?>
+
+	</body>
+</html>
