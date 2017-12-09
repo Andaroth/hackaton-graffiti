e5fb8ae05b723261baadb1332584fcbe5c7d41fb
Valerian Thomas <thomasvalerian93@gmail.com>
2017-12-09 18:04:00 +0100

    adding the map in main

--- php/views/main.php
+++ php/views/main.php
@@ -1,8 +1,82 @@
 <!-- Main -->
-				<div class="wrapper style2">
+
+<html>
+<head>
+	<title>Custom Markers</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+	<meta charset="utf-8">
+	<style>
+		/* Always set the map height explicitly to define the size of the div
+		 * element that contains the map. */
+		#map {
+			height: 100%;
+		}
+		/* Optional: Makes the sample page fill the window. */
+		html, body {
+			height: 100%;
+			margin: 0;
+			padding: 0;
+		}
+	</style>
+</head>
+<body>
+	<div id="map"></div>
+	<script>
+		var map;
+		function initMap() {
+			map = new google.maps.Map(document.getElementById('map'), {
+				zoom: 16,
+				center: new google.maps.LatLng(50.850346, 4.351721),
+				mapTypeId: 'roadmap'
+			});
+
+			var iconBase = '../../images/ico/';
+			var icons = {
+				graff: {
+					icon: iconBase+'paint.png'
+				},
+				bd: {
+					icon:iconBase+'paintbrush.png'
+				}
+			};
+
+			var features = [
+				{
+					position: new google.maps.LatLng(50.845409, 4.350503),
+					type: 'bd',
+					name:'tintin'
+				}, {
+					position: new google.maps.LatLng(50.846520, 4.350350),
+					type: 'bd',
+					name:'le passage'
+				}, {
+					position: new google.maps.LatLng(50.847814, 4.361705),
+					type: 'bd'
+				}
+			];
+
+			// Create markers.
+			features.forEach(function(feature) {
+				var marker = new google.maps.Marker({
+					position: feature.position,
+					icon: icons[feature.type].icon,
+					map: map
+				});
+			});
+		}
+	</script>
+	<script async defer
+	src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCt_zMT2EJuzj0cUnHZ_BHX7DqB9RTU2n8&callback=initMap">
+	</script>
+</body>
+</html>
+
+
+
+				<!-- <div class="wrapper style2">
 
 					<article id="main" class="container special">
-						<a href="#" class="image featured"><img src="images/pic06.jpg" alt="" /></a>
+
 						<header>
 							<h2><a href="#">Sed massa imperdiet magnis</a></h2>
 							<p>
@@ -25,4 +99,4 @@
 						</footer>
 					</article>
 
-				</div>
+				</div> -->
