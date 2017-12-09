286d7257f747e750eab6965fff088aa8552c10a2
Valerian Thomas <thomasvalerian93@gmail.com>
2017-12-09 22:05:01 +0100

    function behind modal working

--- php/views/main.php
+++ php/views/main.php
@@ -18,30 +18,13 @@
 			padding: 0;
 		}
 	</style>
+	<link rel="stylesheet" href="../../assets/css/remodal-default-theme.css"/>
+	<link rel="stylesheet" href="../../assets/css/remodal.css"/>
 </head>
 <body>
 	<div id="map"></div>
 	<script>
-	var contentString = '<div id="content">'+
-            '<div id="siteNotice">'+
-            '</div>'+
-            '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
-            '<div id="bodyContent">'+
-            '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
-            'sandstone rock formation in the southern part of the '+
-            'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
-            'south west of the nearest large town, Alice Springs; 450&#160;km '+
-            '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
-            'features of the Uluru - Kata Tjuta National Park. Uluru is '+
-            'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
-            'Aboriginal people of the area. It has many springs, waterholes, '+
-            'rock caves and ancient paintings. Uluru is listed as a World '+
-            'Heritage Site.</p>'+
-            '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
-            'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
-            '(last visited June 22, 2009).</p>'+
-            '</div>'+
-            '</div>';
+
 
 		var map;
 		function initMap() {
@@ -65,39 +48,53 @@
 				{
 					position: new google.maps.LatLng(50.845409, 4.350503),
 					type: 'bd',
-					name:'tintin'
+					name:'tintin',
+					url:'',
+					descritpion:'',
+					id:''
+
 				}, {
 					position: new google.maps.LatLng(50.846520, 4.350350),
 					type: 'bd',
-					name:'le passage'
+					name:'le passage',
+					url:'',
+					descritpion:'',
+					id:''
 				}, {
 					position: new google.maps.LatLng(50.847814, 4.361705),
-					type: 'bd'
+					type: 'bd',
+					name:'ta maman',
+					url:'',
+					descritpion:'',
+					id:''
 				}
 			];
 
 			// Create markers.
+			function modal(feature){
+				document.write('<div class="remodal" data-remodal-id='+feature.id+'><button data-remodal-action="close" class="remodal-close"></button><h1>'+feature.name+'</h1><p>'+feature.descritpion+'</p><img style="width:100;" src='+feature.url+'" alt=""><button data-remodal-action="cancel" class="remodal-cancel">Cancel</button><button data-remodal-action="confirm" class="remodal-confirm">OK</button></div>')
+			}
 			features.forEach(function(feature) {
-				var infowindow = new google.maps.InfoWindow({
-          content: contentString
-        });
+
+
 				var marker = new google.maps.Marker({
 					position: feature.position,
 					icon: icons[feature.type].icon,
 					map: map
 				});
-				marker.addListener('click', function() {
-	          infowindow.open(map, marker);
-	        });
-			});
+
+				marker.addListener('click', (feature)=>modal(feature));
 
 
 
-		}
+
+		});
+	}
 	</script>
 	<script async defer
 	src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCt_zMT2EJuzj0cUnHZ_BHX7DqB9RTU2n8&callback=initMap">
 	</script>
+	<!-- <script src="../../assets/js/remodal.js"></script> -->
 </body>
 </html>
 
